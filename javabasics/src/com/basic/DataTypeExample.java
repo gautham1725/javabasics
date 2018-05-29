aad09f2acd316f3b0bc852917efd08dc70c999b2
gautham1725 <gautham1725@gmail.com>
2018-05-29 15:56:53 -0700

    basics

--- javabasics/src/com/basic/DataTypeExample.java
+++ javabasics/src/com/basic/DataTypeExample.java
@@ -3,7 +3,8 @@ package com.basic;
 public class DataTypeExample {
 public static void main(String[] args)
 {
-	String firstName="Gowtham";
-	String lastName="Subramaniyum";
+	String firstName = "Gowtham";
+	String lastName = "Subramaniyum";
+	System.out.println(firstName +" "+ lastName);
 }
 }
