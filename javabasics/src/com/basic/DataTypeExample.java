fd45fd56fda492e9feab7f427e0b711dbc0bc766
gautham1725 <gautham1725@gmail.com>
2018-05-29 18:50:39 -0700

    LoopingStatements

--- javabasics/src/com/basic/DataTypeExample.java
+++ javabasics/src/com/basic/DataTypeExample.java
@@ -26,6 +26,7 @@ public static void main(String[] args)
 
 	System.out.println(2 ^ 3);
 	System.out.println( istrue & confirm() );
+//	confirm() == true ;
 	
 }
 
--- javabasics/src/com/basic/LoopingStatements.java
+++ javabasics/src/com/basic/LoopingStatements.java
@@ -1,5 +1,36 @@
 package com.basic;
 
 public class LoopingStatements {
-
+	public static void main (String[] args) {
+		
+    int count = 20;
+     if ( count > 20 )
+     {
+     System.out.println(30);
+     }
+     else if (count < 20)
+     {
+    	 System.out.println(10);
+     }
+     else
+    	 System.out.println(20);
+     
+     int responce = count > 20 ? 30 : 10;
+     
+     System.out.println("switch");
+     int choice = 2;
+     switch(choice) {
+     case 1:
+    	 System.out.println(1);
+    	// break;
+     case 2:
+    	 System.out.println(2);
+    	// break;
+    	 default:
+    		 System.out.println("default");
+    		 //break;
+     }
+    	 
+	}
 }
+
