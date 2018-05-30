15f2c7c42571b04ad06870fa2da9e9b6ba0cc105
gautham1725 <gautham1725@gmail.com>
2018-05-30 16:37:59 +0530

    LoopingStatements

--- javabasics/src/com/basic/DataTypeExample.java
+++ javabasics/src/com/basic/DataTypeExample.java
@@ -3,38 +3,10 @@ package com.basic;
 public class DataTypeExample {
 public static void main(String[] args)
 {
-	String firstName = "Gowtham";
-	String lastName = "Subramaniyum";
-	System.out.println(firstName +" "+ lastName);
-	
-	int count = 10;
-	boolean istrue = false ;
-	count ++ ;
-	System.out.println(count);
-	
-	System.out.println(count > 20);
-	System.out.println(count > 10);
-	
-	System.out.println(count >= 20);
-	System.out.println(count <= 20);
-	
-	System.out.println(count == 20);
-	System.out.println(count != 20);
-	
-	System.out.println(2 & 3);
-	System.out.println(2 | 3);
-
-	System.out.println(2 ^ 3);
-	System.out.println( istrue & confirm() );
-//	confirm() == true ;
-	
-}
-
-		public static boolean confirm()
-		{
-			System.out.println(" confirm() called ");
-			return true;
-		}
-	
 		
+	
+	
+	}
 }
+
+
--- javabasics/src/com/basic/LoopingStatements.java
+++ javabasics/src/com/basic/LoopingStatements.java
@@ -3,7 +3,7 @@ package com.basic;
 public class LoopingStatements {
 	public static void main (String[] args) {
 		
-    int count = 20;
+   /* int count = 20;
      if ( count > 20 )
      {
      System.out.println(30);
@@ -18,8 +18,8 @@ public class LoopingStatements {
      int responce = count > 20 ? 30 : 10;
      
      System.out.println("switch");
-     int choice = 2;
-     switch(choice) {
+     int choose = 2;
+     switch(choose) {
      case 1:
     	 System.out.println(1);
     	// break;
@@ -30,7 +30,67 @@ public class LoopingStatements {
     		 System.out.println("default");
     		 //break;
      }
+    	 System.out.println("switch2");
     	 
+    	 int gowtham = 1;
+    	 switch(gowtham) {
+    	 case 1:
+    		 System.out.println(1);
+    		 break;
+    	 case 2:
+    		 System.out.println(2);
+    		 break;
+    		 default:
+    			 System.out.println(3);
+    			 break;
+    			 }
+    	 
+    	 int i = 0;
+    		while ( i < 12 ) {
+    		   System.out.println(i);
+    		   i++;
+    		}
+    		
+    		
+    	      i = 0;
+    		do {
+    			System.out.println("i = " + i);
+    			i++;
+    		}while(i<10);
+    		
+    		
+    		for(i=0;i<10;i++) {
+    			System.out.println("for loop ="+ i);
+    			
+    		}
+    		
+    		for(i=0;i<10;i++) {
+    			if(i==2) {
+    				continue;
+    			}
+    			System.out.println("for loop ="+ i);
+    				if(i==7) {
+    					break;
+    				}
+    			}
+    		*/
+    		
+    		
+ outer :   		for(int j=0;j<5;j++) {
+ lower :   			for(int k=0;k<7;k++) {
+	                    if(k==5) {
+	                    	continue outer ;
+	                    }
+	                    if(k==3) {
+	                    	continue lower ;
+	                    }
+	                    System.out.println("j = "+j  +  " k ="+k);
+	                   
+    				
+    			}
+    		}
+    			
+    			
 	}
 }
 
